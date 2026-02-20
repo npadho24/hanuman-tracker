4fc301cc43aaf9f469c4fd1eb5a379e8a5684efa
npadho24 <npadho24@gmail.com>
2026-02-20 23:42:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,3 +23,19 @@ function setReminder() {
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
 }
+
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  document.getElementById("installBtn").style.display = "block";
+});
+
+document.getElementById("installBtn").addEventListener("click", () => {
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then(() => {
+    deferredPrompt = null;
+  });
+});
